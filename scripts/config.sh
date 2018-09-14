76e5ba503690891ce2a3b2beb6d44684259805ae
Luc <email@luc-blanc.com>
2018-09-14 11:52:21 +0200

    20180914

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,6 +5,8 @@ sed -i 's/rest_server = rest-ip/rest_server = '${DCSSVR}'/' /etc/datacore/dataco
 sed -i 's/user = user/user = '${DCSUNAME}'/' /etc/datacore/datacore_get_perf.ini && \
 sed -i 's/passwd = pass/passwd = '${DCSPWORD}'/' /etc/datacore/datacore_get_perf.ini
 
+/etc/init.d/grafana-server start
+
 sleep 10
 
 echo "Create Influxdb DataCore database"
